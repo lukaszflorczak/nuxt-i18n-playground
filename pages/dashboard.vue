270fe190dad5e070c43a0b48d39556eab1ac75db
lukaszflorczak <lukasz@florczak.dev>
2023-07-11 08:05:44 +0200

    Reproduce bug with `defineI18nRoute(false)` in beta.13

---
+++ pages/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dashboard</h1>
+
+  <p>
+    <nuxt-link :to="localePath('/')">Back to home</nuxt-link>
+  </p>
+</template>
+
+<script lang="ts" setup>
+  defineI18nRoute(false)
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
   <h1>{{ $t('hello') }}</h1>
+
+  <p>
+    <nuxt-link to="/dashboard">Go to dashboard</nuxt-link>
+  </p>
 </template>
